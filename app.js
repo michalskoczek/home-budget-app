c470cc37d0004fdbb6322147fbd2f05b855c9ab8
Michał Skoczek <mwskoczek@gmail.com>
2021-01-25 19:57:15 +0100

    Added function connectDB to app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/.env' });
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 const app = express();
 
