1d778a0b499a56dcfbfa50861741e651cbb6b0c7
Michał Skoczek <mwskoczek@gmail.com>
2021-01-31 14:47:41 +0100

    Resolved conflict

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ connectDB();
 
 const app = express();
 
-<<<<<<< HEAD
-app.use('/', mainRouter);
-=======
 app.use('/', homepageRouter);
->>>>>>> main-routes
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
