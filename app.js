eb090a53d16cb878263c4a35b6359a2e1abed377
Michał Skoczek <mwskoczek@gmail.com>
2021-01-31 14:41:45 +0100

    removed app.get /

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const mainRouter = require('./routes/main');
+const homepageRouter = require('./routes/homepage');
 
 dotenv.config({ path: './config/.env' });
 const PORT = process.env.PORT || 3000;
@@ -10,10 +10,6 @@ connectDB();
 
 const app = express();
 
-app.use('/', mainRouter);
-
-app.get('/', (req, res) => {
-  res.send('Hello Michał');
-});
+app.use('/', homepageRouter);
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
