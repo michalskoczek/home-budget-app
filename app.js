6556e5e60e39ec40a5941eb17a508724a2866463
Michał Skoczek <mwskoczek@gmail.com>
2021-02-02 21:52:54 +0100

    Added new route - register

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const homepageRouter = require('./routes/homepage');
+const registerRouter = require('./routes/register');
 
 dotenv.config({ path: './config/.env' });
 const PORT = process.env.PORT || 3000;
@@ -9,7 +10,9 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 const app = express();
+app.use(express.json());
 
 app.use('/', homepageRouter);
+app.use('/register', registerRouter);
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
