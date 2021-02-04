752b60016497f70d2713537cdabee30f59ceba79
Michał Skoczek <mwskoczek@gmail.com>
2021-02-04 21:48:28 +0100

    Added async await to router.post

--- routes/register.js
+++ routes/register.js
@@ -2,13 +2,19 @@ const express = require('express');
 const router = express.Router();
 const User = require('../model/User');
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
   });
-  user.save();
+
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 module.exports = router;
