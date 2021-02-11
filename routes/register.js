ed254f54a08a8b9ea232f59c3a1f5ac802e136da
Michał Skoczek <mwskoczek@gmail.com>
2021-02-11 22:44:19 +0100

    Added joi schema

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const Joi = require('joi');
 const router = express.Router();
 const User = require('../model/User');
 
+const schema = Joi.object({
+  name: Joi.string().min(3).max(255).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).max(1024).required(),
+  confirmPassword: Joi.ref('password'),
+});
+
 router.post('/', async (req, res) => {
-  const user = new User({
+  const validation = schema.validate(req.body);
+  res.send(validation);
+
+  /* const user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
@@ -14,7 +25,7 @@ router.post('/', async (req, res) => {
     res.send(savedUser);
   } catch (error) {
     res.status(400).send(error);
-  }
+  } */
 });
 
 module.exports = router;
