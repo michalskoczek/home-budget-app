8a7f55b4d405e5c5bd8873c09f08b21de7a8da0a
Michał Skoczek <mwskoczek@gmail.com>
2021-02-15 21:36:19 +0100

    Added package bcrypt to script

--- routes/register.js
+++ routes/register.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const User = require('../model/User');
 const { registerValidation } = require('../validation');
 
