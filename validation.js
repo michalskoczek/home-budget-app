8434f65c069ebae6b5a2d99d8afe78d42586e6cd
Michał Skoczek <mwskoczek@gmail.com>
2021-02-16 21:53:52 +0100

    Created function loginValidation

--- validation.js
+++ validation.js
@@ -11,4 +11,13 @@ const registerValidation = (data) => {
   return schema.validate(data);
 };
 
+const loginValidation = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).max(1024).required(),
+  });
+
+  return schema.validate(data);
+};
 module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
