f62403b7010afca8fefbe988b9111c9dd731bd23
Michał Skoczek <mwskoczek@gmail.com>
2021-02-22 22:00:43 +0100

    Required jsonwebtoken

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../model/User');
 const { registerValidation, loginValidation } = require('../validation');
 
