a80a7705a24034588ad62af9c453cbe702630fa0
Michał Skoczek <mwskoczek@gmail.com>
2021-03-03 21:40:25 +0100

    Added app.use flickity

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: false }), express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '/node_modules/bootstrap/dist')));
 app.use(express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use(express.static(path.join(__dirname, '/node_modules/flickity/dist')));
 
 app.use('/budget', budgetRouter);
 
