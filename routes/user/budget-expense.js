2366648ea90d1b5aa2d2ec1c5d9e4e9a75816ab1
Michał Skoczek <mwskoczek@gmail.com>
2021-03-11 21:38:13 +0100

    Added new key - budget to function render

--- routes/user/budget-expense.js
+++ routes/user/budget-expense.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('user/budget-expense', { pageTitle: 'Home Budget App' });
+  res.render('user/budget-expense', {
+    pageTitle: 'Home Budget App',
+    budget: true,
+  });
 });
 
 module.exports = router;
