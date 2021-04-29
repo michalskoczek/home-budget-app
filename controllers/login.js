26cfad9e9e77c909b08cafbed4c1f3193e3ca7a4
Michał Skoczek <mwskoczek@gmail.com>
2021-04-29 13:40:27 +0200

    Added handling error - wrong password

--- controllers/login.js
+++ controllers/login.js
@@ -39,7 +39,14 @@ exports.postLoginAuth = async (req, res) => {
   })
 
   const validPass = await bcrypt.compare(req.body.password, user.password);
-  if (!validPass) return res.status(400).send('Password is invalid');
+  if (!validPass){ return res.render('login', {
+    pageTitle: 'Home Budget App',
+    successfulResgistration: false,
+    error: true,
+    messageError: 'Password is invalid',
+  }) } else {
+    return res.redirect('../user/budget')
+  }
 
   const accessToken = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
   res.header('auth-token', accessToken).send(accessToken);
