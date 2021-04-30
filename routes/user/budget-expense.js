274853167b90dfcc7445ff3f8f13bab1b0997a6e
Michał Skoczek <mwskoczek@gmail.com>
2021-04-30 13:42:52 +0200

    Added path to object res.render

--- routes/user/budget-expense.js
+++ routes/user/budget-expense.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.render('user/budget-expense', {
     pageTitle: 'Home Budget App',
+    path: '/user',
     isLogged: req.session.isLogged,
   });
 });
