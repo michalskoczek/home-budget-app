f5caca679225f000e645a8bfda96cacaef6f3705
Michał Skoczek <mwskoczek@gmail.com>
2021-04-30 20:18:51 +0200

    Updated properties in error-page render, added path and isLogged

--- controllers/error.js
+++ controllers/error.js
@@ -1,5 +1,7 @@
 exports.getErrorPage = (req, res) => {
-  res
-    .status(404)
-    .render('error-page', { errorPageTitle: 'Error | Home Budget App' });
+  res.status(404).render('error-page', {
+    errorPageTitle: 'Error | Home Budget App',
+    path: '/',
+    isLogged: req.session.isLogged,
+  });
 };
