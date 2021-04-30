99ea559710228df83c70b62d1d65cdb81f9805ce
Michał Skoczek <mwskoczek@gmail.com>
2021-04-30 12:49:18 +0200

    Updated login controller file - used req.session

--- controllers/login.js
+++ controllers/login.js
@@ -4,13 +4,13 @@ const User = require('../model/User');
 const { loginValidation } = require('../validation');
 
 exports.getLoginPage = (req, res) => {
-   res.render('login', {
+  res.render('login', {
     pageTitle: 'Home Budget App',
     error: false,
     successfulResgistration: false,
     messageRegistration: 'Your profile has just created!',
   });
-}
+};
 
 exports.getLoggedPage = (req, res) => {
   res.render('login', {
@@ -18,34 +18,39 @@ exports.getLoggedPage = (req, res) => {
     error: false,
     successfulResgistration: true,
     messageRegistration: 'Your profile has just created!',
-  }); 
-}
+  });
+};
 
 exports.postLoginAuth = async (req, res) => {
   const { error } = loginValidation(req.body);
-  if (error) return res.render('login', {
-    pageTitle: 'Home Budget App',
-    successfulResgistration: false,
-    error: true,
-    messageError: error.details[0].message,
-  })
+  if (error)
+    return res.render('login', {
+      pageTitle: 'Home Budget App',
+      successfulResgistration: false,
+      error: true,
+      messageError: error.details[0].message,
+    });
 
   const user = await User.findOne({ email: req.body.email });
-  if (!user) return res.render('login', {
-    pageTitle: 'Home Budget App',
-    successfulResgistration: false,
-    error: true,
-    messageError: 'Email is not found',
-  })
+  if (!user)
+    return res.render('login', {
+      pageTitle: 'Home Budget App',
+      successfulResgistration: false,
+      error: true,
+      messageError: 'Email is not found',
+    });
 
   const validPass = await bcrypt.compare(req.body.password, user.password);
-  if (!validPass){ return res.render('login', {
-    pageTitle: 'Home Budget App',
-    successfulResgistration: false,
-    error: true,
-    messageError: 'Password is invalid',
-  }) } else {
-    return res.redirect('../user/budget')
+  if (!validPass) {
+    return res.render('login', {
+      pageTitle: 'Home Budget App',
+      successfulResgistration: false,
+      error: true,
+      messageError: 'Password is invalid',
+    });
+  } else {
+    req.session.isLogged = true;
+    return res.redirect('../user/budget');
   }
 
   // const accessToken = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
