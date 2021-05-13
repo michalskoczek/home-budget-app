663ad4102b7376850075c497509b7db236cb40c9
Michał Skoczek <mwskoczek@gmail.com>
2021-05-13 14:11:22 +0200

    Moved callback function to another file - budget-expense controller

--- routes/user/budget-expense.js
+++ routes/user/budget-expense.js
@@ -1,24 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userNotLogged = require('../../middleware/userNotLogged.js');
+const userController = require('../../controllers/budget-expense');
 
-router.get('/:name', userNotLogged, (req, res) => {
-  if (req.params.name !== req.session.userName)
-    return res.redirect(`/user/${req.session.userName}`);
-
-  let successfulMessage = req.flash('successfulMessage');
-  if (successfulMessage.length > 0) {
-    successfulMessage = successfulMessage[0];
-  } else {
-    successfulMessage = null;
-  }
-  res.render('user/budget-expense', {
-    pageTitle: 'Home Budget App',
-    path: '/user',
-    userName: req.session.userName,
-    successfulMessageFlash: successfulMessage,
-    errorMessageFlash: null,
-  });
-});
+router.get('/:name', userNotLogged, userController.getBudgetExpense);
 
 module.exports = router;
