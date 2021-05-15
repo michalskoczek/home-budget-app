6a654379769b7e93c4d9404d3b80167e5964e389
Michał Skoczek <mwskoczek@gmail.com>
2021-05-15 23:22:38 +0200

    Changed name of file budget-expense

--- routes/user/budget-expense.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userNotLogged = require('../../middleware/userNotLogged.js');
-const userController = require('../../controllers/budget-expense');
-
-router.get('/:name', userNotLogged, userController.getBudgetExpense);
-
-module.exports = router;
