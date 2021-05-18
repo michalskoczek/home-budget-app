5d9b6c4317455a6e044badd00d59098cea5bd1d4
Michał Skoczek <mwskoczek@gmail.com>
2021-05-18 21:15:43 +0200

    Updated expenseSchema, added userId

--- model/Expense.js
+++ model/Expense.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const expenseSchema = new Schema({
+  userId: { type: String },
   title: { type: String, minLength: 1 },
   amount: { type: Number },
   date: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Budget', budgetSchema);
+module.exports = mongoose.model('Expense', expenseSchema);
