88e9f82b9dc3ff7db4bdd887a623c992b6a06026
Michał Skoczek <mwskoczek@gmail.com>
2021-07-31 16:33:16 +0200

    Updated name of correct expense button and deleted unnecessary code

--- public/js/user-page.js
+++ public/js/user-page.js
@@ -1,66 +1,12 @@
-const updateBtn = document.querySelector('.form__update-budget-btn');
-const budgetInfoSpan = document.querySelector('.information__amount--budget');
-const budgetInput = document.querySelector('input[name="budget"]');
-
 const addExpenseBtn = document.querySelector('.form__add-expense-btn');
-const expenseInfoSpan = document.querySelector('.information__amount--expense');
 const expenseTitleInput = document.querySelector('input[name="title"]');
 const expenseAmountInput = document.querySelector('input[name="expense"]');
-
-const balanceInfoSpan = document.querySelector('.information__amount--balance');
-
-const budget = new Budget();
-const expense = new Expense();
-
-updateBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-
-  if (budgetInput.value === '') {
-    return;
-  }
-
-  budget.isValueCorrect(Number(budgetInput.value));
-  budget.updateBudget(budgetInfoSpan);
-  budget.checkBalance(expenseInfoSpan, balanceInfoSpan);
-  document.querySelector('#form-budget').submit();
-  budgetInput.value = '';
-});
-
-addExpenseBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-
-  if (
-    expenseTitleInput.value === '' ||
-    expenseAmountInput.value === '' ||
-    expenseAmountInput.value < 0 ||
-    expenseAmountInput.value == 0
-  )
-    return;
-
-  expense.isExpenseCorrect(
-    expenseTitleInput.value,
-    Number(expenseAmountInput.value),
-  );
-
-  // expense.updateExpense(expenseInfoSpan);
-  // expense.updateBalance(
-  //   balanceInfoSpan,
-  //   Number(budgetInfoSpan.textContent),
-  //   Number(expenseInfoSpan.textContent),
-  // );
-
-  //expense.addExpenseToTable();
-  console.log('add expense submit');
-  document.querySelector('#form-expense').submit();
-  expenseTitleInput.value = '';
-  expenseAmountInput.value = '';
-});
 
 const createButtonCorrectExpense = () => {
   const buttonCorrectExpense = document.createElement('button');
   buttonCorrectExpense.className = 'form__add-expense-btn btn btn-danger';
   buttonCorrectExpense.id = 'update-expense';
-  buttonCorrectExpense.innerText = 'Update expense';
+  buttonCorrectExpense.innerText = 'Correct expense';
   buttonCorrectExpense.setAttribute('type', 'button');
   document.querySelector('#expenses form').appendChild(buttonCorrectExpense);
 };
@@ -69,7 +15,6 @@ const editExpenseButton = [...document.querySelectorAll('#edit')];
 
 editExpenseButton.forEach((element, index) => {
   element.addEventListener('click', () => {
-    console.log('edit click' + index);
     addExpenseBtn.remove();
     createButtonCorrectExpense();
 
