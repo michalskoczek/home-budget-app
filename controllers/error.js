609d81eeff0de0a14ff11f4704fe9e806fb8937d
Michał Skoczek <mwskoczek@gmail.com>
2021-08-02 14:49:02 +0200

    Created getError500Page function to handle errors

--- controllers/error.js
+++ controllers/error.js
@@ -6,3 +6,12 @@ exports.getErrorPage = (req, res) => {
     userName: req.session.userName,
   });
 };
+
+exports.getError500Page = (req, res) => {
+  res.status(500).render('error-500-page', {
+    errorPageTitle: 'Error | Home Budget App',
+    path: '/',
+    isLogged: req.session.isLogged,
+    userName: req.session.userName,
+  });
+};
